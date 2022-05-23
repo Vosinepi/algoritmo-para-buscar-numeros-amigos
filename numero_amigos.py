a4a1515e0314b1ebe1dc44167d567b51a22b6844
Vosinepi <ismaelpiovani@gmail.com>
2022-05-23 12:28:12 -0300

    Numero amigos v1

---
+++ numero_amigos.py
@@ -0,0 +1,45 @@
+def numeros_amigos(inicio, fin):
+    """
+    It takes a range of numbers and prints out the pairs of amicable numbers within that range
+
+    :param inicio: the starting number
+    :param fin: the number you want to stop at
+    """
+
+    for numero in range(inicio, fin):
+
+        numero_1 = numero
+
+        # Finding the sum of the divisors of the number 1.
+        contador_1 = 0
+        divisores_1 = []
+        for divisor in range(1, numero_1):
+
+            if numero_1 % divisor == 0:
+                divisores_1.append(divisor)
+                contador_1 += 1
+        suma_divisores_1 = sum(divisores_1)
+
+        numero_2 = suma_divisores_1
+
+        # Finding the sum of the divisors of the number 2.
+        contador_2 = 0
+        divisores_2 = []
+        for divisor in range(1, numero_2):
+
+            if numero_2 % divisor == 0:
+                divisores_2.append(divisor)
+                contador_2 += 1
+        suma_divisores_2 = sum(divisores_2)
+
+        # Checking if the sum of the divisors of the second number is equal to the first number. If it
+        # is, it checks if the first number is not equal to the second number. If it is not, it checks
+        # if the first number is less than the second number. If it is, it prints out the pair of
+        # numbers.
+        if suma_divisores_2 == numero:
+            if numero != numero_2:
+                if numero < numero_2:
+                    print(numero, numero_2)
+
+
+numeros_amigos(1, 500)
---
+++ readme.md
@@ -0,0 +1,2 @@
+Pequeño algortimo para buscar numeros amigos dentro de un intervalo dado.
+Cuanto mas grande el intervalo y mas grandes los numeros mas tarda el calculo.
